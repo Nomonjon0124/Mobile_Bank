314a0de3ab1a6b9181b8f082c04d5f89d1ed60de
Nomonjon <ummugulusum2425@gmail.com>
2023-10-20 17:19:38 +0500

    Splash oynalar bitdi, Binding qio'shildi

--- app/src/main/java/com/ummug/mobilebank/ui/SplashFragment1.kt
+++ app/src/main/java/com/ummug/mobilebank/ui/SplashFragment1.kt
@@ -2,6 +2,7 @@ package com.ummug.mobilebank.ui
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.ummug.mobilebank.R
@@ -15,16 +16,16 @@ class SplashFragment1: Fragment(R.layout.fragment_splash1) {
 
         object :CountDownTimer(3000,1000){
             override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
                 parentFragmentManager.beginTransaction()
                     .setReorderingAllowed(true)
                     .replace(R.id.container,SplashFragment2())
                     .commit()
             }
 
-            override fun onFinish() {
-
-            }
-
-        }
+        }.start()
     }
 }
--- app/src/main/java/com/ummug/mobilebank/ui/SplashFragment2.kt
+++ app/src/main/java/com/ummug/mobilebank/ui/SplashFragment2.kt
@@ -1,7 +1,39 @@
 package com.ummug.mobilebank.ui
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ummug.mobilebank.R
+import com.ummug.mobilebank.databinding.FragmentSplash2Binding
 
 class SplashFragment2:Fragment(R.layout.fragment_splash2) {
+    private var _binding:FragmentSplash2Binding?=null ;
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding=FragmentSplash2Binding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+
+    @SuppressLint("ResourceAsColor")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            checkbox.setOnClickListener {
+                if(checkbox.isChecked){
+                    bottom.setBackgroundResource(R.color.button_color)
+                }
+                else if (!checkbox.isChecked){
+                    bottom.setBackgroundResource(R.color.button_color_defoult)
+                }
+            }
+        }
+    }
 }
