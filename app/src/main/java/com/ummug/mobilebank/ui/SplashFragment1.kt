5900250ad978d845cedfea049bd6fc9553cfba11
AbduvaliyevSamandar <abduvaliyev145@gmail.com>
2023-10-20 17:21:34 -0700

    Modullar yozildi hamma oyna uchun fffdddss

--- app/src/main/java/com/ummug/mobilebank/ui/SplashFragment1.kt
+++ app/src/main/java/com/ummug/mobilebank/ui/SplashFragment1.kt
@@ -2,7 +2,6 @@ package com.ummug.mobilebank.ui
 
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.ummug.mobilebank.R
@@ -16,16 +15,16 @@ class SplashFragment1: Fragment(R.layout.fragment_splash1) {
 
         object :CountDownTimer(3000,1000){
             override fun onTick(p0: Long) {
-
-            }
-
-            override fun onFinish() {
                 parentFragmentManager.beginTransaction()
                     .setReorderingAllowed(true)
                     .replace(R.id.container,SplashFragment2())
                     .commit()
             }
 
-        }.start()
+            override fun onFinish() {
+
+            }
+
+        }
     }
 }
