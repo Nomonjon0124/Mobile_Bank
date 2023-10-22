3b9ee5b94d5ae2d3235e77785969227fa86af998
AbduvaliyevSamandar <abduvaliyev145@gmail.com>
2023-10-21 20:58:31 -0700

    Auth qismi bajarildi endi pin code qismiga otish qismini qilamiz iloji bolsa ozgartirmaylar qoshsalrin mayli iltimos bollardd

--- app/src/main/java/com/ummug/mobilebank/ui/Register/RegisterFragment.kt
+++ app/src/main/java/com/ummug/mobilebank/ui/Register/RegisterFragment.kt
@@ -77,7 +77,6 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
         }
 
     private val openVerifyLiveDataObserver: Observer<Unit> = Observer {
-
         Toast.makeText(requireContext(), "Verifyni och", Toast.LENGTH_SHORT).show()
             parentFragmentManager.beginTransaction()
                 .replace(R.id.container,VerificationFragment())
