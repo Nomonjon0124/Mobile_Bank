33f9a137e6cda12104d45b2170d69685682de3c0
behruzbek2Gulomov <120156394+behruzbek2Gulomov@users.noreply.github.com>
2023-11-25 21:19:56 +0500

    Behruz

--- app/src/main/java/com/ummug/mobilebank/ui/History/HistoryBottomSheedFragment.kt
+++ app/src/main/java/com/ummug/mobilebank/ui/History/HistoryBottomSheedFragment.kt
@@ -62,6 +62,14 @@ class HistoryBottomSheedFragment : BottomSheetDialogFragment() {
         return super.onCreateDialog(savedInstanceState)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.listHistory()
